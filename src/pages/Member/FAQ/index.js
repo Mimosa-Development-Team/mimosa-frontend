413b63e0988c68934059a7a916143628a93d3507
Krissel Parming <krissel@offshorly.com>
2021-02-17 16:09:26 +0800

    task: initial layout

---
+++ src/pages/Member/FAQ/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+const MemberFAQ = () => {
+  return (
+    <div>
+      <Button color="primary">Hello World</Button>
+    </div>
+  )
+}
+
+MemberFAQ.propTypes = {}
+
+export default MemberFAQ
