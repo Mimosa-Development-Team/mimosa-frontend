4dac0f2766e7fff8f1c29e61d7d76c808612032d
Krissel Parming <krissel@offshorly.com>
2021-02-18 18:50:11 +0800

    task: added SearchField component

--- src/pages/Member/FAQ/components/Accordion.js
+++ src/pages/Member/FAQ/components/Accordion.js
@@ -3,18 +3,35 @@ import Typography from '@material-ui/core/Typography'
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
+import RemoveIcon from '@material-ui/icons/Remove'
 import styles from './styles.module.scss'
 
 const TabPanel = props => {
   const { title, content } = props
+  const [expanded, setExpanded] = React.useState(false)
+
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false)
+  }
 
   return (
     <div className={`${styles.accordionWrapper}`}>
-      <Accordion className={`${styles.accordion}`}>
+      <Accordion
+        className={`${styles.accordion}`}
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
         <AccordionSummary
           className={`${styles.accordionSummary}`}
-          expandIcon={<ExpandMoreIcon />}
+          expandIcon={
+            expanded === 'panel1' ? (
+              // temp inline style - set colors usable in components @k
+              <RemoveIcon style={{ color: '#EF8C20' }} />
+            ) : (
+              <AddIcon style={{ color: '#EF8C20' }} />
+            )
+          }
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
