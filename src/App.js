0a62807145485cb340dffb1e6dfcb3106d189a9a
cloydalcantara <alcantaracloy26@gmail.com>
2021-03-03 08:58:32 +0800

    update: login design and set logging in status

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { QueryClientProvider } from 'react-query'
 import { isEmpty } from 'lodash'
 import Login from 'pages/Login'
 import Member from 'pages/Member'
-
+// import IdleTimer from 'react-idle-timer'
 import { Routes } from 'global/routes'
 
 import { queryClient, useGlobalState } from 'store/state'
@@ -13,9 +13,32 @@ const App = () => {
   const { user } = useGlobalState()
   const hasSession = !isEmpty(user)
 
+  // const onAction = e => {
+  //   console.log('user did something', e)
+  // }
+
+  // const onActive = e => {
+  //   console.log('user is active', e)
+  //   // console.log("time remaining", this.idleTimer.getRemainingTime());
+  // }
+
+  // const onIdle = e => {
+  //   console.log('user is idle', e)
+  //   // console.log("last active", this.idleTimer.getLastActiveTime());
+  // }
+
   const homeComponent = hasSession ? Member : Login
   return (
     <BrowserRouter>
+      {/* <IdleTimer
+        // ref={ref => { Null = ref }}
+        element={document}
+        onActive={onActive}
+        onIdle={onIdle}
+        onAction={onAction}
+        debounce={250}
+        timeout={1}
+      /> */}
       <QueryClientProvider client={queryClient}>
         <Switch>
           <Route
