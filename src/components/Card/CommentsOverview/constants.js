7a380054c8e6c9351b7b63e585d3652631fcaa74
Lui Aranes <louiejie@offshorly.com>
2021-03-25 11:01:34 +0800

    comment post api integration

--- src/components/Card/CommentsOverview/constants.js
+++ src/components/Card/CommentsOverview/constants.js
@@ -1,1 +1,2 @@
 export const COMMENT_QUERY_KEY = 'comment/get'
+export const COMMENT_POST_QUERY_KEY = 'comment/post'
--- src/components/Card/CommentsOverview/index.js
+++ src/components/Card/CommentsOverview/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import Avatar from '@material-ui/core/Avatar'
-// import Controls from 'components/controls/Controls'
+import Controls from 'components/controls/Controls'
+import { useForm } from 'react-hook-form'
 import { useGlobalState } from 'store/state'
 import getRawData from 'utils/parsing/Proxy'
 import Comment from './Comment'
@@ -10,7 +11,29 @@ import styles from './styles.module.scss'
 const CommentsOverview = ({ contributionId }) => {
   const { user } = useGlobalState()
 
-  const { comments, getComments } = useComments(contributionId)
+  const {
+    comments,
+    getComments,
+    addComment,
+    addedComment,
+    addLoadingComment,
+    addErrorComment
+  } = useComments(contributionId)
+
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      comment: ''
+    }
+  })
+
+  const onSubmit = data => {
+    const formFields = {
+      comment: data.comment,
+      contributionId,
+      userId: getRawData(user).user.id
+    }
+    addComment(formFields)
+  }
 
   useEffect(() => {
     getComments()
@@ -22,12 +45,18 @@ const CommentsOverview = ({ contributionId }) => {
         <Avatar className={`${styles.avatar}`}>
           {getRawData(user).user.firstName.charAt(0)}
         </Avatar>
-        {/* <Controls.Input
-          className={`${styles.input}`}
-          name="comment"
-          placeholder="Write a comment..."
-          background="offwhite"
-        /> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controls.Input
+            className={`${styles.input}`}
+            name="comment"
+            control={control}
+            placeholder="Write a comment..."
+            background="offwhite"
+            addedComment={addedComment}
+            addLoadingComment={addLoadingComment}
+            addErrorComment={addErrorComment}
+          />
+        </form>
       </div>
       {(comments || []).map(data => {
         return (
