be10489b49fa3625682c3d8781926a83628d2552
Lui Aranes <louiejie@offshorly.com>
2021-03-30 15:11:28 +0800

    add loader and adjust margins of card

--- src/pages/Member/Home/index.js
+++ src/pages/Member/Home/index.js
@@ -7,13 +7,14 @@ import SearchField from 'components/SearchField'
 import { Button } from '@material-ui/core'
 import AddBoxIcon from '@material-ui/icons/AddBox'
 import Typography from '@material-ui/core/Typography'
+import loader from 'assets/images/loader_loading.gif'
 import { ROUTES } from '../constants'
 import styles from './style.module.scss'
 import { useQuestions } from './hooks'
 
 const MemberDashboard = () => {
   const history = useHistory()
-  const { questions, getQuestions } = useQuestions()
+  const { questions, isLoading, getQuestions } = useQuestions()
 
   useEffect(() => {
     getQuestions()
@@ -22,50 +23,56 @@ const MemberDashboard = () => {
   return (
     <>
       <LeftSidebar showNav links={ROUTES} />
-      <PageContentWrapper>
-        <div className={`${styles.groupHeader}`}>
-          <SearchField className={`${styles.searchBox}`} />
-          <Button
-            className={`${styles.questionBtn}`}
-            size="large"
-            variant="contained"
-            onClick={() => {
-              history.push('/contribution-form/question/new', {
-                type: 'new'
-              })
-            }}
-          >
-            <AddBoxIcon /> NEW QUESTION
-          </Button>
+      {isLoading ? (
+        <div className="loaderWrapper">
+          <img src={loader} alt="Loading ..." />
         </div>
-        {questions ? (
-          <>
-            <div className={`${styles.paperListHeader}`}>
-              <Typography
-                className={`${styles.title}`}
-                variant="h5"
-              >
-                Paper List
-              </Typography>
-            </div>
-            {(questions || []).map(data => (
-              <div
-                className={`${styles.content}`}
-                onClick={() => {
-                  history.push(`/contribution/${data.uuid}`)
-                }}
-              >
-                <Card
-                  data={data}
-                  form={false}
-                  hideDetails
-                  hideEdit
-                />
+      ) : (
+        <PageContentWrapper>
+          <div className={`${styles.groupHeader}`}>
+            <SearchField className={`${styles.searchBox}`} />
+            <Button
+              className={`${styles.questionBtn}`}
+              size="large"
+              variant="contained"
+              onClick={() => {
+                history.push('/contribution-form/question/new', {
+                  type: 'new'
+                })
+              }}
+            >
+              <AddBoxIcon /> NEW QUESTION
+            </Button>
+          </div>
+          {questions ? (
+            <>
+              <div className={`${styles.paperListHeader}`}>
+                <Typography
+                  className={`${styles.title}`}
+                  variant="h5"
+                >
+                  Paper List
+                </Typography>
               </div>
-            ))}
-          </>
-        ) : null}
-      </PageContentWrapper>
+              {(questions || []).map(data => (
+                <div
+                  className={`${styles.content}`}
+                  onClick={() => {
+                    history.push(`/contribution/${data.uuid}`)
+                  }}
+                >
+                  <Card
+                    data={data}
+                    form={false}
+                    hideDetails
+                    hideEdit
+                  />
+                </div>
+              ))}
+            </>
+          ) : null}
+        </PageContentWrapper>
+      )}
     </>
   )
 }
