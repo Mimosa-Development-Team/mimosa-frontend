deb6985999212b9276547250864e3923c0298e43
Krissel Parming <krissel@offshorly.com>
2021-04-01 11:45:24 +0800

    additional ui issue fixes

--- src/pages/Member/ContributionForm/index.js
+++ src/pages/Member/ContributionForm/index.js
@@ -2,8 +2,9 @@
 import React, { useEffect, useState } from 'react'
 import { useGlobalState } from 'store/state'
 import getRawData from 'utils/parsing/Proxy'
+import LeftSidebar from 'components/LeftSidebar'
+import PageContentWrapper from 'components/PageContentWrapper'
 import Contribution from './components/contribution'
-import styles from './style.module.scss'
 import { useQuestionForm } from './hooks'
 
 const ContributionForm = props => {
@@ -56,38 +57,49 @@ const ContributionForm = props => {
   ])
 
   return (
-    <div className={`${styles.homeWrapper}`}>
-      <Contribution
-        profile={getRawData(user).user}
-        tagsData={tagsData}
-        relatedMediaData={relatedMediaData}
-        addedData={addedContribution}
-        updatedData={updatedContribution}
-        questionUuid={location.state.questionUuid}
-        userData={userData}
-        type={match.params.type}
-        method={match.params.method}
-        data={location.state.data ? location.state.data : null}
-        addedContribution={addedContribution}
-        addErrorContribution={addErrorContribution}
-        updatedContribution={updatedContribution}
-        updateIsLoadingContribution={updateIsLoadingContribution}
-        updateErrorContribution={updateErrorContribution}
-        addLoadingContribution={addLoadingContribution}
-        addIsSuccessContribution={addIsSuccessContribution}
-        updateIsSuccessContribution={updateIsSuccessContribution}
-        addContribution={addContribution}
-        updateContribution={updateContribution}
-        getRelatedMedia={getRelatedMedia}
-        resetAdd={resetAdd}
-        resetUpdate={resetUpdate}
-        deleteContribution={deleteContribution}
-        deleteIsLoadingContribution={deleteIsLoadingContribution}
-        deleteErrorContribution={deleteErrorContribution}
-        deleteMutate={deleteMutate}
-        deleteIsSuccessContribution={deleteIsSuccessContribution}
-      />
-    </div>
+    <>
+      <LeftSidebar />
+      <PageContentWrapper>
+        <Contribution
+          profile={getRawData(user).user}
+          tagsData={tagsData}
+          relatedMediaData={relatedMediaData}
+          addedData={addedContribution}
+          updatedData={updatedContribution}
+          questionUuid={location.state.questionUuid}
+          userData={userData}
+          type={match.params.type}
+          method={match.params.method}
+          data={location.state.data ? location.state.data : null}
+          addedContribution={addedContribution}
+          addErrorContribution={addErrorContribution}
+          updatedContribution={updatedContribution}
+          updateIsLoadingContribution={
+            updateIsLoadingContribution
+          }
+          updateErrorContribution={updateErrorContribution}
+          addLoadingContribution={addLoadingContribution}
+          addIsSuccessContribution={addIsSuccessContribution}
+          updateIsSuccessContribution={
+            updateIsSuccessContribution
+          }
+          addContribution={addContribution}
+          updateContribution={updateContribution}
+          getRelatedMedia={getRelatedMedia}
+          resetAdd={resetAdd}
+          resetUpdate={resetUpdate}
+          deleteContribution={deleteContribution}
+          deleteIsLoadingContribution={
+            deleteIsLoadingContribution
+          }
+          deleteErrorContribution={deleteErrorContribution}
+          deleteMutate={deleteMutate}
+          deleteIsSuccessContribution={
+            deleteIsSuccessContribution
+          }
+        />
+      </PageContentWrapper>
+    </>
   )
 }
 
