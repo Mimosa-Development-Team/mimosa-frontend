ac33889f77adacf29e9b00d0fbef48ea19bf7bea
Lui Aranes <louiejie@offshorly.com>
2021-04-27 08:59:10 +0800

    infinite scroll on my contribution list

--- src/pages/Member/UserContributions/index.js
+++ src/pages/Member/UserContributions/index.js
@@ -5,12 +5,12 @@ import { useGlobalState } from 'store/state'
 import getRawData from 'utils/parsing/Proxy'
 
 import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
 
 import PageWrapper from 'components/PageWrapper'
 import PageContentWrapper from 'components/PageContentWrapper'
 import Card from 'components/Card'
 import SortFilter from 'components/SortFilter'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 import loader from 'assets/images/loader_loading.gif'
 
@@ -30,11 +30,16 @@ const UserContributions = () => {
     isLoading,
     refetch,
     getUserContributions,
-    hasNextPage,
-    isFetchingNextPage
+    hasNextPage
   } = useUserContributions(orderBy, getRawData(user).user.id)
 
   const [sort, setSort] = useState('Most Recent')
+
+  const dataLength = userContributions
+    ? userContributions.pages.reduce((counter, page) => {
+        return counter + page.contributions.length
+      }, 0)
+    : ''
 
   const handleSortClick = orderBy => {
     if (orderBy === 'DESC') {
@@ -57,88 +62,87 @@ const UserContributions = () => {
         </div>
       ) : (
         <PageContentWrapper>
-          <Typography className="mb-20" variant="h1">
-            My Contributions
-          </Typography>
-          {userContributions ? (
-            <Dashboard
-              question={userContributions.pages[0].questionCtr}
-              hypothesis={
-                userContributions.pages[0].hypothesisCtr
-              }
-              experiment={
-                userContributions.pages[0].experimentCtr
-              }
-              data={userContributions.pages[0].dataCtr}
-              analysis={userContributions.pages[0].analysisCtr}
-            />
-          ) : null}
-          <>
+          <InfiniteScroll
+            dataLength={dataLength}
+            next={getUserContributions}
+            hasMore={hasNextPage}
+            loader={
+              <div className={`${styles.loadingLabel}`}>
+                Loading...
+              </div>
+            }
+            scrollableTarget="scrollableList"
+          >
+            <Typography className="mb-20" variant="h1">
+              My Contributions
+            </Typography>
             {userContributions ? (
-              <>
-                <div className={`${styles.paperListHeader}`}>
-                  <Typography
-                    className={`${styles.title}`}
-                    variant="h5"
-                  >
-                    Contribution List
-                    {` (${userContributions.pages[0].totalContributions})`}
-                  </Typography>
-                  <div className={`${styles.sortWrapper}`}>
+              <Dashboard
+                question={userContributions.pages[0].questionCtr}
+                hypothesis={
+                  userContributions.pages[0].hypothesisCtr
+                }
+                experiment={
+                  userContributions.pages[0].experimentCtr
+                }
+                data={userContributions.pages[0].dataCtr}
+                analysis={userContributions.pages[0].analysisCtr}
+              />
+            ) : null}
+            <>
+              {userContributions ? (
+                <>
+                  <div className={`${styles.paperListHeader}`}>
                     <Typography
                       className={`${styles.title}`}
                       variant="h5"
                     >
-                      Sort By:
+                      Contribution List
+                      {` (${userContributions.pages[0].totalContributions})`}
                     </Typography>
-                    <SortFilter
-                      sortFilter={sort}
-                      onClick={handleSortClick}
-                    />
-                  </div>
-                </div>
-                {userContributions.pages.map((group, i) => (
-                  <React.Fragment key={i}>
-                    {/* {JSON.stringify(group.contributions)} */}
-                    {(group.contributions || []).map(data => (
-                      <div
-                        className={`${styles.content}`}
-                        onClick={() => {
-                          history.push(
-                            `/contribution/${
-                              data.parentQuestionUuid ||
-                              data.uuid
-                            }`,
-                            { state: data }
-                          )
-                        }}
+                    <div className={`${styles.sortWrapper}`}>
+                      <Typography
+                        className={`${styles.title}`}
+                        variant="h5"
                       >
-                        <Card
-                          data={data}
-                          form={false}
-                          linesToShow={5}
-                          parentTitle={data.parentTitle}
-                        />
-                      </div>
-                    ))}
-                  </React.Fragment>
-                ))}
-                <div className={`${styles.loadMore}`}>
-                  <Button
-                    className={`${styles.loadMoreBtn}`}
-                    onClick={() => getUserContributions()}
-                    disabled={!hasNextPage || isFetchingNextPage}
-                  >
-                    {isFetchingNextPage
-                      ? 'Loading more...'
-                      : hasNextPage
-                      ? 'Load More'
-                      : 'Nothing more to load'}
-                  </Button>
-                </div>
-              </>
-            ) : null}
-          </>
+                        Sort By:
+                      </Typography>
+                      <SortFilter
+                        sortFilter={sort}
+                        onClick={handleSortClick}
+                      />
+                    </div>
+                  </div>
+                  {userContributions.pages.map((group, i) => (
+                    <React.Fragment key={i}>
+                      {/* {JSON.stringify(group.contributions)} */}
+                      {(group.contributions || []).map(data => (
+                        <div
+                          className={`${styles.content}`}
+                          onClick={() => {
+                            history.push(
+                              `/contribution/${
+                                data.parentQuestionUuid ||
+                                data.uuid
+                              }`,
+                              { state: data }
+                            )
+                          }}
+                        >
+                          <Card
+                            data={data}
+                            form={false}
+                            linesToShow={5}
+                            parentTitle={data.parentTitle}
+                          />
+                        </div>
+                      ))}
+                    </React.Fragment>
+                  ))}
+                </>
+              ) : null}
+            </>
+          </InfiniteScroll>
         </PageContentWrapper>
       )}
     </PageWrapper>
