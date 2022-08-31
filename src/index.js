24a065c8e33c741e15618a8a45edafafabbd2d30
Cloyd Alcantara <78535926+cloydma@users.noreply.github.com>
2022-08-31 12:02:37 +0800

    revert app.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import 'global/style-config/fonts.scss'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<div>
-  <UsersnapClassicLoader apikey="03b2bc23-2106-49a5-b7c6-1d4fd4edc720" />
-  <App />
-</div>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.unregister()
